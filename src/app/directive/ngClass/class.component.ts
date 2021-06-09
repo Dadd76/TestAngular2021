bd050b92c8678c0fcfa657ec19691f8414d7d058
Yannick <yannick.darcillon@gmail.com>
2021-06-09 14:42:37 +0200

    directive ngClass

--- src/app/directive/ngClass/class.component.ts
+++ src/app/directive/ngClass/class.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-class',
+  selector: 'app-ngclass',
   templateUrl: './class.component.html',
   styleUrls: ['./class.component.css']
 })
 export class ClassComponent implements OnInit {
 
+  Red = true;
+  Blue = false;
+  Purple = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changecColor(color : string)
+  {
+  if(color == "red")
+    this.Red = true != this.Purple !=  this.Blue ;
+  else if(color == "blue")
+    this.Blue = true != this.Red != this.Purple;
+  else if(color=="purple")
+    this.Purple = true != this.Red != this.Blue;
+
   }
 
 }
